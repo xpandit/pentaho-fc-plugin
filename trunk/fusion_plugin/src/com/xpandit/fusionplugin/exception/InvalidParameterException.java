aad07ac678502ccc688d220798ab831e34fb8309
david.duque@xpand-it.com <david.duque@xpand-it.com@cf0f7267-cd06-1477-91e6-b5b8ed27f82b>
2011-03-10 01:04:07 +0000

    Issue 2

--- trunk/fusion_plugin/src/com/xpandit/fusionplugin/exception/InvalidParameterException.java
+++ trunk/fusion_plugin/src/com/xpandit/fusionplugin/exception/InvalidParameterException.java
@@ -13,11 +13,11 @@ public class InvalidParameterException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public static final String ERROR_001 = "Parameter Chart Type Required";
-	public static final String ERROR_002 = "Invalid Parameter Chart Type";
-	public static final String ERROR_003 = "Parameter Required";
-	public static final String ERROR_004 = "Factory cant load this chart type";
-	public static final String ERROR_005 = "Invalid Parameter";
+	public static final String ERROR_001 = "Parameter Chart Type Required:";
+	public static final String ERROR_002 = "Invalid Parameter Chart Type:";
+	public static final String ERROR_003 = "Parameter Required:";
+	public static final String ERROR_004 = "Factory cant load this chart type:";
+	public static final String ERROR_005 = "Invalid Parameter:";
 	public static final String ERROR_006 = "Parameter Required:";
 
 	/**
