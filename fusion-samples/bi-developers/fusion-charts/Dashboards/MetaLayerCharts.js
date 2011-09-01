054521444015b9b035ddceff0ac4089901b50e10
david.duque@xpand-it.com <david.duque@xpand-it.com@cf0f7267-cd06-1477-91e6-b5b8ed27f82b>
2011-09-01 15:04:47 +0000

    revert; and Issue 35;

--- fusion-samples/bi-developers/fusion-charts/Dashboards/MetaLayerCharts.js
+++ fusion-samples/bi-developers/fusion-charts/Dashboards/MetaLayerCharts.js
@@ -3,23 +3,18 @@
 		width: "400",
 		height: "150",
 		chartType: "Line",
-		xFusionPath: "bi-developers/fusion-charts/Fusion Charts Free/LineChart.xfusion",
 		cdaDataAccessId:"2"
 	},
-	
 	pieChartDef: {
 		width: "400",
 		height: "150",
 		chartType: "Pie3D",
-		xFusionPath: "bi-developers/fusion-charts/Fusion Charts Free/PieChart3D.xfusion",
 		cdaDataAccessId:"2"
 	},
-	
 	columnChartDef: {
 		width: "400",
 		height: "150",
 		chartType: "Column3D",
-		xFusionPath: "bi-developers/fusion-charts/Fusion Charts Free/ColumnChart3D.xfusion",
 		cdaDataAccessId:"2"
 	},
 	
@@ -27,7 +22,6 @@
 		width: "400",
 		height: "150",
 		chartType: "MSColumn3D",
-		xFusionPath: "bi-developers/fusion-charts/Fusion Charts Free/MSColumnChart3D.xfusion",
 		cdaDataAccessId:"2"
 	},
 	
@@ -35,15 +29,12 @@
 		width: "400",
 		height: "150",
 		chartType: "Bar2D",
-		xFusionPath: "bi-developers/fusion-charts/Fusion Charts Free/BarChart2D.xfusion",
 		cdaDataAccessId:"2"
 	},
-	
 	drillBarChartDef: {
 		width: "400",
 		height: "150",
 		chartType: "Bar2D",
-		xFusionPath: "bi-developers/fusion-charts/Fusion Charts Free/DrillBarChart2D.xfusion",
 		seriesParam:"SeriesParamName",
 		categoriesParam:"categoriesParamName",
 		chartLink:"JavaScript:drillBarChart.drillFunction('{SeriesParamName}','{categoriesParamName}')", 	
