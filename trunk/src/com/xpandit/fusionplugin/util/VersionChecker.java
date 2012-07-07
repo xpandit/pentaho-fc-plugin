d718c6cac40505a919f260c073467c16045bac2c
david.duque@xpand-it.com <david.duque@xpand-it.com@cf0f7267-cd06-1477-91e6-b5b8ed27f82b>
2012-07-07 10:12:26 +0000

    Issue 60 Documentation

--- trunk/src/com/xpandit/fusionplugin/util/VersionChecker.java
+++ trunk/src/com/xpandit/fusionplugin/util/VersionChecker.java
@@ -7,13 +7,21 @@ import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.versionchecker.util.VersionInfo;
 import org.pentaho.versionchecker.util.VersionHelper;
 
+/**
+ * 
+ * This class gets the version of pentaho platform an components  
+ * 
+ * @author DGPG
+ *
+ */
 public class VersionChecker {
 
 	
 	static VersionInfo versionInfoPentahoSystem = VersionHelper.getVersionInfo(PentahoSystem.class);
 
+	
 	public static void getVersions(OutputStream out) throws IOException {
-		 
+		//generate a JS variable to be evaluated on client side
 		out.write(("XDashFusionChartComponent.pentahoVersion={MajorVersion:"+versionInfoPentahoSystem.getVersionMajor()+",MinorVersion:"+versionInfoPentahoSystem.getVersionMinor()+"}").getBytes());
 	}	
 }
