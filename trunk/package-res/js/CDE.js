a1d545b7534822132315561982c4b2a936e7c3ed
david.duque@xpand-it.com <david.duque@xpand-it.com@cf0f7267-cd06-1477-91e6-b5b8ed27f82b>
2014-01-14 10:09:54 +0000

    Issue 118:	Upgrade plugin to be compatible with Pentaho 5

---
+++ trunk/package-res/js/CDE.js
@@ -0,0 +1,5 @@
+var XDashFusionChartComponentAngularGauge = XDashFusionChartComponent.extend({type: "XDashFusionChartComponentAngularGauge"});
+var XDashFusionChartComponentRealTimeColumn = XDashFusionChartComponent.extend({type: "XDashFusionChartComponentRealTimeColumn"});
+var XDashFusionChartComponentRealTimeLine = XDashFusionChartComponent.extend({type: "XDashFusionChartComponentRealTimeLine"});
+var XDashFusionChartComponentRealTimeLineDY = XDashFusionChartComponent.extend({type: "XDashFusionChartComponentRealTimeLineDY"});
+var XDashFusionChartComponentRealTimeStackedArea = XDashFusionChartComponent.extend({type: "XDashFusionChartComponentRealTimeStackedArea"});
