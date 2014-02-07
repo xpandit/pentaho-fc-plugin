fe601e191de9153152de7a39908045e860640bcf
laura.oliveira@xpand-it.com <laura.oliveira@xpand-it.com@cf0f7267-cd06-1477-91e6-b5b8ed27f82b>
2014-02-07 11:51:13 +0000

    -Key generator package name fixed.

--- src/com/xpandit/fusionplugin/pentaho/FcLifecycleListener.java
+++ src/com/xpandit/fusionplugin/pentaho/FcLifecycleListener.java
@@ -1,21 +1,7 @@
 package com.xpandit.fusionplugin.pentaho;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Date;
-import java.util.List;
-
 import org.pentaho.platform.api.engine.IPluginLifecycleListener;
 import org.pentaho.platform.api.engine.PluginLifecycleException;
-import org.pentaho.platform.engine.core.system.PentahoSystem;
-
-import pt.portoeditora.pebook.serial.SerialData;
-import pt.portoeditora.pebook.serial.SerialGenerator;
-
 import com.xpandit.fusionplugin.GlobalPropertiesManager;
 
 /**
