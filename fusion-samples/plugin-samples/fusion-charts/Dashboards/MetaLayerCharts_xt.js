b8e0082a98a91f63f42fe112d961be3f9c09b865
Bruno Ribeiro <bruno-ribeiro@xpand-it.com>
2015-09-08 17:39:23 +0100

    Closed #148 Added FusionCharts XT example CDF dashboard

---
+++ fusion-samples/plugin-samples/fusion-charts/Dashboards/MetaLayerCharts_xt.js
@@ -0,0 +1,25 @@
+﻿var MetaLayerCharts_xt = {
+	LineChart: {
+		height          : "200",
+		width           : "500",
+		chartType       : "Line",
+		cdaPath         : "/public/plugin-samples/fusion-charts/Fusion Charts/LineChart.cda",
+		cdaDataAccessId : "2",
+		chartTitle      : "Sales Per year",
+		xAxisName       : "Years",
+		yAxisName       : "Sales",
+		free            : "false",
+	},
+	
+	PieChart2D: {
+		height          : "200",
+		width           : "500",
+		chartType       : "Pie2D",
+		cdaPath         : "/public/plugin-samples/fusion-charts/Fusion Charts/PieChart2D.cda",
+		cdaDataAccessId : "2",
+		chartTitle      : "Sales Per year",
+		xAxisName       : "Years",
+		yAxisName       : "Sales",
+		free            : "false",
+	},
+};
